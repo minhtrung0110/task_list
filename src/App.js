c2658b92bd896571294dc62447eb076429a7ef4c
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2022-12-16 00:21:57 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,141 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="trello-minhtrung-master">
+      <div className="navbar app"> App bar</div>
+      <div className="navbar board">Board bar </div>
+      <div className="board-columns ">
+          <div className="column">
+              <header>Brainstorm + </header>
+              <ul>
+                  <li>
+                      <img src="https://placeimg.com/640/480/any" alt='img'/>
+                      Title: Minh Trung
+                  </li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+
+              </ul>
+              <footer>Add another card</footer>
+          </div>
+          <div className="column">
+              <header>Brainstorm + </header>
+              <ul>
+                  <li>
+                      <img src="https://placeimg.com/640/480/any" alt='img'/>
+                      Title: Minh Trung
+                  </li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+
+              </ul>
+              <footer>Add another card</footer>
+          </div>
+          <div className="column">
+              <header>Brainstorm + </header>
+              <ul>
+                  <li>
+                      <img src="https://placeimg.com/640/480/any" alt='img'/>
+                      Title: Minh Trung
+                  </li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+
+              </ul>
+              <footer>Add another card</footer>
+          </div>
+          <div className="column">
+              <header>Brainstorm + </header>
+              <ul>
+                  <li>
+                      <img src="https://placeimg.com/640/480/any" alt='img'/>
+                      Title: Minh Trung
+                  </li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+
+              </ul>
+              <footer>Add another card</footer>
+          </div>
+          <div className="column">
+              <header>Brainstorm + </header>
+              <ul>
+                  <li>
+                      <img src="https://placeimg.com/640/480/any" alt='img'/>
+                      Title: Minh Trung
+                  </li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+
+              </ul>
+              <footer>Add another card</footer>
+          </div>
+          <div className="column">
+              <header>Brainstorm + </header>
+              <ul>
+                  <li>
+                      <img src="https://placeimg.com/640/480/any" alt='img'/>
+                      Title: Minh Trung
+                  </li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+
+              </ul>
+              <footer>Add another card</footer>
+          </div>
+          <div className="column">
+              <header>Brainstorm + </header>
+              <ul>
+                  <li>
+                      <img src="https://placeimg.com/640/480/any" alt='img'/>
+                      Title: Minh Trung
+                  </li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+                  <li>Authen cho chức năng đăng nhập và tạo đề thi và thi</li>
+
+              </ul>
+              <footer>Add another card</footer>
+          </div>
+
+
+      </div>
     </div>
   );
 }
