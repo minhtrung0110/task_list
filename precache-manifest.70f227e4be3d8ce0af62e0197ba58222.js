6613714ad29ae8a3bc4cf1e6a5015b7c6a2ac621
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2022-12-23 19:32:22 +0700

    Updates

---
+++ precache-manifest.70f227e4be3d8ce0af62e0197ba58222.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b744c3bc48278ec35224",
+    "url": "/task_list/static/css/main.234f4baf.chunk.css"
+  },
+  {
+    "revision": "b744c3bc48278ec35224",
+    "url": "/task_list/static/js/main.b744c3bc.chunk.js"
+  },
+  {
+    "revision": "e222dc63630413f36758",
+    "url": "/task_list/static/js/1.e222dc63.chunk.js"
+  },
+  {
+    "revision": "8a68763c365ae8294f5d",
+    "url": "/task_list/static/js/2.8a68763c.chunk.js"
+  },
+  {
+    "revision": "c3861942c9fb61c0ad9f",
+    "url": "/task_list/static/js/runtime~main.c3861942.js"
+  },
+  {
+    "revision": "dcb860027d4951328b339efe2fa4c14b",
+    "url": "/task_list/index.html"
+  }
+];
