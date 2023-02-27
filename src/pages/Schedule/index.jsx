5187dd058e6c43e37c1ca2f8eeec41ca3e2247d5
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2023-02-27 16:18:25 +0700

    build events property Calendar

--- src/pages/Schedule/index.jsx
+++ src/pages/Schedule/index.jsx
@@ -5,19 +5,64 @@ import {Calendar,
     DateLocalizer,
     momentLocalizer} from "react-big-calendar";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-
+import './style.scss'
 function SchedulePage(props) {
     const [myEvents, setEvents] = useState([])
+    const events = [
+        {
+            title: 'Sự kiện A',
+            start: new Date(),
+            end: new Date(),
+            description: 'Mô tả sự kiện A'
+        },
+        {
+                title: 'Họp Dự Án ',
+                type: 'event',
+                description: '',
+                file:'',
+                members: [],
+                notifications:true,
+                repeats:true,
+            start: new Date(),
+            end: new Date(),
+            },
+        {
+            title: 'Sự kiện B',
+            start: new Date(),
+            end: new Date(),
+            description: 'Mô tả sự kiện B'
+        },
+        // ...
+    ];
+
     // const events = [
     //     {
-    //         title: 'Event 1',
+    //         title: 'Họp Dự Án ',
+    //         type: 'event',
+    //         description: '',
+    //         file:'',
+    //         members: [],
+    //         notifications:true,
+    //         repeats:true,
     //         start: moment('2023-02-25 09:00:00'),
     //         end: moment('2023-02-25 10:00:00'),
     //     },
     //     {
-    //         title: 'Event 2',
-    //         start: moment('2023-02-26 14:00:00'),
-    //         end: moment('2023-02-26 15:30:00'),
+    //         title: 'Hoàn Thành Lịch Biểu ',
+    //         type: 'todo',
+    //         description: '',
+    //         notifications:true,
+    //         repeats:true,
+    //         start: moment('2023-02-25 09:00:00'),
+    //         end: moment('2023-02-25 10:00:00'),
+    //     },
+    //     {
+    //         title: 'Đóng Tiền Nước ',
+    //         type: 'reminder',
+    //         notifications:true,
+    //         repeats:true,
+    //         start: moment('2023-02-25 09:00:00'),
+    //         end: moment('2023-02-25 10:00:00'),
     //     },
     // ];
     const localizer = momentLocalizer(moment);
@@ -26,14 +71,17 @@ function SchedulePage(props) {
         ({ start, end }) => {
             const title = window.prompt('New Event Name')
             if (title) {
-                setEvents((prev) => [...prev, { start, end, title }])
+                setEvents((prev) => [...prev, { start, end, title, allDay:true }])
             }
         },
         [setEvents]
     )
 
     const handleSelectEvent = useCallback(
-        (event) => window.alert(event.title),
+        (event) => {
+            window.alert(event.title)
+            console.log(event)
+        },
         []
     )
     const { defaultDate, scrollToTime } = useMemo(
@@ -43,17 +91,42 @@ function SchedulePage(props) {
         }),
         []
     )
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        const style = {
+            backgroundColor: event.color,
+            borderRadius: '5px',
+            opacity: 0.8,
+            color: 'white',
+            border: '0px',
+            display: 'block'
+        };
+        return {
+            style: style,
+            title: event.title,
+            type:event.type,
+            description: event.description,
+            files:event.files,
+            members:event.members,
+            notification:event.notification,
+            repeats:event.repeat,
+        };
+    };
     return (
         <div className='minhtrung-content-calendar'>
+            <div className='sidebar'>
+
+            </div>
             <Calendar
+                className='calendar'
                 defaultDate={defaultDate}
                 defaultView={Views.MONTH}
-                events={myEvents}
+                events={events}
                 localizer={localizer}
                 onSelectEvent={handleSelectEvent}
                 onSelectSlot={handleSelectSlot}
                 selectable
                 scrollToTime={scrollToTime}
+                eventPropGetter={eventStyleGetter}
             />
         </div>
     );
